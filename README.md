c280b1378bd6328efd262528da61ed1051d73200
overlookmotel <theoverlookmotel@gmail.com>
2018-04-28 00:21:19 +0100

    Initial release

--- README.md
+++ README.md
@@ -11,7 +11,124 @@
 [![Greenkeeper badge](https://badges.greenkeeper.io/overlookmotel/stream-gen.svg)](https://greenkeeper.io/)
 [![Coverage Status](https://img.shields.io/coveralls/overlookmotel/stream-gen/master.svg)](https://coveralls.io/r/overlookmotel/stream-gen)
 
+## Purpose
+
+Streams can be hard to test.
+
+You want to test transform streams etc with large batches of data to ensure they're robust, but you don't want to include big files to test with in the repo.
+
+This module provides:
+
+* Readable stream which streams out data of whatever size you need
+* Writable stream which receives data and checks it's what it should be
+
+This is achieved entirely in memory (no file streams etc) but with minimal memory use.
+
+The streams are created from a generator function you provide, which can produce streams of whatever content and size you require.
+
 ## Usage
+
+### Installation
+
+```
+npm install stream-gen
+```
+
+### Classes
+
+#### GeneratorReadStream( gen [, options] )
+
+Readable stream that gets its content from a generator.
+
+The generator must `yield` a byte value (0-255) each time it is called. The content of the stream is made up of these bytes.
+
+```js
+const { GeneratorReadStream } = require('stream-gen');
+
+// Data generator - creates 1KB of data
+function* gen() {
+  for (let i = 0; i < 1024; i++) {
+    yield i % 256;
+  }
+}
+
+const producer = new GeneratorReadStream( gen );
+
+producer.pipe( destination );
+```
+
+#### GeneratorWriteStream( gen [, options], callback )
+
+Writable stream that receives content and compares them to result of a generator. It calls `callback` with result of the comparison.
+
+The generator must be of same type as for `GeneratorReadStream`.
+
+```js
+const { GeneratorWriteStream } = require('stream-gen');
+
+const checker = new GeneratorWriteStream( gen, function(err) {
+  if (err) return console.log('Stream differed');
+  console.log('Stream as expected');
+} );
+
+source.pipe( checker );
+```
+
+### Putting it together
+
+The two parts work together to test that a transfer of data has been completed without altering the data.
+
+Let's say you are testing a lossless compression component.
+
+```
+Input -> Compress -> Decompress -> Output
+```
+
+Input and output should be the same.
+
+You can test this with:
+
+```js
+const { GeneratorReadStream, GeneratorWriteStream } = require('stream-gen');
+const zlib = require('zlib');
+
+// Data generator - creates 100MB of data
+function* gen() {
+	for (let i = 0; i < 100 * 1024 * 1024; i++) {
+		yield i % 256;
+	}
+}
+
+// Create producer and checker
+const producer = new GeneratorReadStream( gen ),
+const checker = new GeneratorWriteStream( gen, function(err) {
+  if (err) throw err;
+  console.log('It works!');
+} );
+
+// Create compressor and decompressor
+const compressor = zlib.createDeflate();
+const decompressor = zlib.createInflate();
+
+// Pipe them all together
+producer.pipe(compressor).pipe(decompressor).pipe(checker);
+```
+
+The callback on `checker` tells us if data after compression and decompression is the same at it started off as.
+
+There we go! We tested ZLIB on 100MB of data, using hardly any memory and no disc access.
+
+### Options
+
+#### GeneratorReadStream
+
+Options `highWaterMark` and `encoding` are passed to Node's `Readable` Stream constructor.
+
+`maxSize` option determines largest size chunk stream will emit.
+
+#### GeneratorWriteStream
+
+Options `highWaterMark` and `emitClose` are passed to Node's `Writable` Stream constructor.
 
 ## Tests
 
--- test/all.test.js
+++ test/all.test.js
@@ -8,6 +8,8 @@
 // Modules
 const chai = require('chai'),
 	{expect} = chai,
+	{Readable, Writable, PassThrough} = require('stream'),
+	zlib = require('zlib'),
 	streamGen = require('../lib/');
 
 // Init
@@ -18,8 +20,144 @@ chai.config.includeStack = true;
 /* jshint expr: true */
 /* global describe, it */
 
-describe('Tests', function() {
-	it.skip('all', function() {
-		expect(streamGen).to.be.ok;
+const { GeneratorReadStream, GeneratorWriteStream } = streamGen;
+
+const LENGTH = 26;
+function* gen() {
+	for (let i = 65; i < 65 + LENGTH; i++) {
+		yield i;
+	}
+}
+
+describe('GeneratorReadStream', function() {
+	it('is subclass of Readable Stream', function() {
+		const s = new GeneratorReadStream(gen);
+		expect(s).to.be.instanceof(Readable);
+	});
+
+	it('produces expected data', function() {
+		const s = new GeneratorReadStream(gen);
+
+		let res = s.read();
+		expect(res.toString()).to.equal('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+		res = s.read();
+		expect(res).to.be.null;
 	});
 });
+
+describe('GeneratorWriteStream', function() {
+	it('is subclass of Writable Stream', function() {
+		const s = new GeneratorWriteStream(gen);
+		expect(s).to.be.instanceof(Writable);
+	});
+
+	it('calls back with no error if streamed data correct', function(cb) {
+		const s = new GeneratorWriteStream(gen, cb);
+
+		s.write('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+		s.end();
+	});
+
+	it('calls back with error if streamed data too short', function(cb) {
+		const str = 'ABCDEFGHIJKLMNOPQRSTUVWXY';
+
+		const s = new GeneratorWriteStream(gen, err => {
+			expect(err).to.be.instanceof(Error);
+			expect(err.message).to.equal(`Not enough data - ended prematurely at ${str.length}`);
+			cb();
+		});
+
+		s.write(str);
+		s.end();
+	});
+
+	it('calls back with error if streamed data too long', function(cb) {
+		const str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ-';
+
+		const s = new GeneratorWriteStream(gen, err => {
+			expect(err).to.be.instanceof(Error);
+			expect(err.message).to.equal(`Too much data - expected ${LENGTH}, got at least ${str.length}`);
+			cb();
+		});
+
+		s.write(str);
+		s.end();
+	});
+
+	it('calls back with error if streamed data different', function(cb) {
+		const str = 'ABCDEFGHIJKL-NOPQRSTUVWXYZ';
+
+		const s = new GeneratorWriteStream(gen, err => {
+			expect(err).to.be.instanceof(Error);
+			expect(err.message).to.match(/^Wrong data - differed in byte range \d+-\d+$/);
+			cb();
+		});
+
+		s.write(str);
+		s.end();
+	});
+});
+
+describe('piped', function() {
+	describe('with short data', function() {
+		runPipeTests(gen);
+	});
+
+	describe('with 1KB data', function() {
+		function* gen() {
+		  for (let i = 0; i < 1000; i++) {
+		    yield i % 256;
+		  }
+		}
+
+		runPipeTests(gen);
+	});
+
+	describe('with 1MB data', function() {
+		function* gen() {
+		  for (let i = 0; i < 1000 * 1000; i++) {
+		    yield i % 256;
+		  }
+		}
+
+		runPipeTests(gen);
+	});
+
+	describe('with 10MB data', function() {
+		function* gen() {
+		  for (let i = 0; i < 10 * 1000 * 1000; i++) {
+		    yield i % 256;
+		  }
+		}
+
+		runPipeTests(gen);
+	});
+});
+
+function runPipeTests(gen) {
+	it('calls back with no error when piped directly', function(cb) {
+		const producer = new GeneratorReadStream(gen),
+		 	checker = new GeneratorWriteStream(gen, cb);
+
+		producer.pipe(checker);
+	});
+
+	it('calls back with no error when piped via passthrough', function(cb) {
+		const producer = new GeneratorReadStream(gen),
+		 	checker = new GeneratorWriteStream(gen, cb);
+
+		const passthrough = new PassThrough();
+
+		producer.pipe(passthrough).pipe(checker);
+	});
+
+	it('calls back with no error when piped via zlib', function(cb) {
+		const producer = new GeneratorReadStream(gen),
+		 	checker = new GeneratorWriteStream(gen, cb);
+
+		const deflate = zlib.createDeflate(),
+			inflate = zlib.createInflate();
+
+		producer.pipe(deflate).pipe(inflate).pipe(checker);
+	});
+}

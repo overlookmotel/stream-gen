4abdadb950c65e16323bdf465b5fde27caa80079
overlookmotel <theoverlookmotel@gmail.com>
2019-09-29 19:30:03 +0100

    Fix lint errors [refactor]

--- test/all.test.js
+++ test/all.test.js
@@ -19,48 +19,48 @@ chai.config.includeStack = true;
 
 /* global describe, it */
 
-const { GeneratorReadStream, GeneratorWriteStream } = streamGen;
+const {GeneratorReadStream, GeneratorWriteStream} = streamGen;
 
 const LENGTH = 26;
-function* gen() {
+function* genLetters() {
 	for (let i = 65; i < 65 + LENGTH; i++) {
 		yield i;
 	}
 }
 
-describe('GeneratorReadStream', function() {
-	it('is subclass of Readable Stream', function() {
-		const s = new GeneratorReadStream(gen);
+describe('GeneratorReadStream', () => {
+	it('is subclass of Readable Stream', () => {
+		const s = new GeneratorReadStream(genLetters);
 		expect(s).to.be.instanceof(Readable);
 	});
 
-	it('produces expected data', function() {
-		const s = new GeneratorReadStream(gen);
+	it('produces expected data', () => {
+		const s = new GeneratorReadStream(genLetters);
 
 		let res = s.read();
 		expect(res.toString()).to.equal('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
 		res = s.read();
-		expect(res).to.be.null;
+		expect(res).to.be.null; // eslint-disable-line no-unused-expressions
 	});
 });
 
-describe('GeneratorWriteStream', function() {
-	it('is subclass of Writable Stream', function() {
-		const s = new GeneratorWriteStream(gen);
+describe('GeneratorWriteStream', () => {
+	it('is subclass of Writable Stream', () => {
+		const s = new GeneratorWriteStream(genLetters);
 		expect(s).to.be.instanceof(Writable);
 	});
 
-	it('calls back with no error if streamed data correct', function(cb) {
-		const s = new GeneratorWriteStream(gen, cb);
+	it('calls back with no error if streamed data correct', (cb) => {
+		const s = new GeneratorWriteStream(genLetters, cb);
 
 		s.write('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
 		s.end();
 	});
 
-	it('calls back with error if streamed data too short', function(cb) {
+	it('calls back with error if streamed data too short', (cb) => {
 		const str = 'ABCDEFGHIJKLMNOPQRSTUVWXY';
 
-		const s = new GeneratorWriteStream(gen, err => {
+		const s = new GeneratorWriteStream(genLetters, (err) => {
 			expect(err).to.be.instanceof(Error);
 			expect(err.message).to.equal(`Not enough data - ended prematurely at ${str.length}`);
 			cb();
@@ -70,10 +70,10 @@ describe('GeneratorWriteStream', function() {
 		s.end();
 	});
 
-	it('calls back with error if streamed data too long', function(cb) {
+	it('calls back with error if streamed data too long', (cb) => {
 		const str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ-';
 
-		const s = new GeneratorWriteStream(gen, err => {
+		const s = new GeneratorWriteStream(genLetters, (err) => {
 			expect(err).to.be.instanceof(Error);
 			expect(err.message).to.equal(`Too much data - expected ${LENGTH}, got at least ${str.length}`);
 			cb();
@@ -83,10 +83,10 @@ describe('GeneratorWriteStream', function() {
 		s.end();
 	});
 
-	it('calls back with error if streamed data different', function(cb) {
+	it('calls back with error if streamed data different', (cb) => {
 		const str = 'ABCDEFGHIJKL-NOPQRSTUVWXYZ';
 
-		const s = new GeneratorWriteStream(gen, err => {
+		const s = new GeneratorWriteStream(genLetters, (err) => {
 			expect(err).to.be.instanceof(Error);
 			expect(err.message).to.match(/^Wrong data - differed in byte range \d+-\d+$/);
 			cb();
@@ -97,12 +97,12 @@ describe('GeneratorWriteStream', function() {
 	});
 });
 
-describe('piped', function() {
-	describe('with short data', function() {
-		runPipeTests(gen);
+describe('piped', () => {
+	describe('with short data', () => {
+		runPipeTests(genLetters);
 	});
 
-	describe('with 1KB data', function() {
+	describe('with 1KB data', () => {
 		function* gen() {
 			for (let i = 0; i < 1000; i++) {
 				yield i % 256;
@@ -112,7 +112,7 @@ describe('piped', function() {
 		runPipeTests(gen);
 	});
 
-	describe('with 1MB data', function() {
+	describe('with 1MB data', () => {
 		function* gen() {
 			for (let i = 0; i < 1000 * 1000; i++) {
 				yield i % 256;
@@ -122,7 +122,7 @@ describe('piped', function() {
 		runPipeTests(gen);
 	});
 
-	describe('with 10MB data', function() {
+	describe('with 10MB data', () => {
 		function* gen() {
 			for (let i = 0; i < 10 * 1000 * 1000; i++) {
 				yield i % 256;
@@ -134,14 +134,14 @@ describe('piped', function() {
 });
 
 function runPipeTests(gen) {
-	it('calls back with no error when piped directly', function(cb) {
+	it('calls back with no error when piped directly', (cb) => {
 		const producer = new GeneratorReadStream(gen),
 			checker = new GeneratorWriteStream(gen, cb);
 
 		producer.pipe(checker);
 	});
 
-	it('calls back with no error when piped via passthrough', function(cb) {
+	it('calls back with no error when piped via passthrough', (cb) => {
 		const producer = new GeneratorReadStream(gen),
 			checker = new GeneratorWriteStream(gen, cb);
 
@@ -150,7 +150,7 @@ function runPipeTests(gen) {
 		producer.pipe(passthrough).pipe(checker);
 	});
 
-	it('calls back with no error when piped via zlib', function(cb) {
+	it('calls back with no error when piped via zlib', (cb) => {
 		const producer = new GeneratorReadStream(gen),
 			checker = new GeneratorWriteStream(gen, cb);
 
